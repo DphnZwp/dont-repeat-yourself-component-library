3d45f80e16ab97aa6a674ecc2881461302df4a88
DphnZwp <zwuupdaphne@gmail.com>
2022-12-14 11:01:03 +0100

    removed clock

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import Link from 'ilojo-bar-components/components/Link.svelte';
   import Header from 'ilojo-bar-components/components/Header.svelte';
   import BigFooter from 'ilojo-bar-components/components/BigFooter.svelte';
-  import Clock from 'ilojo-bar-components/components/Clock.svelte';
 	import '../styles/global.css';
 </script>
 
@@ -10,7 +9,6 @@
 	<Link linkTitle="Home" link="/" />
 	<Link linkTitle="Discover stories" link="/" />
 	<br>
-	<Clock />
 </Header>
 
 <main>
